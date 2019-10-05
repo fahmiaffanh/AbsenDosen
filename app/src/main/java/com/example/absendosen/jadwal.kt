671256bb0cd024e8100ea26e7a77f755f2c9794d
fahmiaffanh <fahmiaffanh@protonmail.com>
2019-10-05 10:30:01 +0700

    dashboard beta

---
+++ app/src/main/java/com/example/absendosen/jadwal.kt
@@ -0,0 +1,12 @@
+package com.example.absendosen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class jadwal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jadwal)
+    }
+}
